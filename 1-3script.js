6ae1e7c96be21b3b3cd7786f9aff136c751f2327
unknown <cmyser.fast@mail.ru>
2021-06-22 16:29:51 +0300

    'first_lesson'

---
+++ 1-3script.js
@@ -0,0 +1,101 @@
+// let d;
+// let r;
+
+// function circleSQRT(d){
+//     let r;
+//     r = d /2;
+//     r = (r**2) * Math.PI;
+//     return window.document.write("<p>" + r  +"</p>");
+// }
+
+// console.log(r);
+// window.document.write("<p>" + r  +"</p>")
+
+// circleSQRT(10)
+
+
+// function aver(){
+    
+//     let i;
+//     let count = 0;
+//     let all = 0;
+//     for ( i = 0; i < arguments.length; i++) {
+        
+//         console.log(typeof(arguments[i]) )
+//         if(typeof(arguments[i]) != typeof(1) ){
+//             console.log('ddddddddddddddddddddddd')
+            
+//         }else{
+//             console.log(all);
+//             all += arguments[i];
+//             count += 1;
+//         }
+        
+        
+        
+        
+        
+        
+//     }
+    
+//     all = all / count;
+//     return all;
+// }
+
+
+// let a = aver(15, 25, 30, 40, 55, '60', 30, '44');
+// console.log(a);
+
+
+
+// let mass = [];
+
+
+// function aver(){
+    
+//     let i;
+//     let count = 0;
+//     let all = 0;
+//     for ( i = 0; i < arguments.length; i++) {
+        
+        
+
+        
+            
+//         if(typeof(arguments[i]) == typeof(mass)){
+            
+//             for (let y =0; y < arguments[i].length; y++){
+//                 if(typeof(arguments[i][y]) != typeof(1) ){
+//                     console.log('ddddddddddddddddddddddd')
+//                     continue    
+//                 }
+//                 all += arguments[i][y] ;
+//                 count += 1;
+                
+//                 console.log(all);
+                
+                
+//             }
+//         } else{
+//             if(typeof(arguments[i]) !== typeof(1) ){
+//                 console.log('ddddddddddddddddddddddd')
+//                 continue
+//             }
+//             all += arguments[i];
+//             count += 1;
+//             console.log(all);
+//         }  
+        
+//     }
+    
+//     all = all / count;
+//     return all;
+// }
+
+
+// let a = aver('[10,20]',[10, '20'], 10, 20,[10, '20'], 30, 40, 50,[10, '20'], '60', '44',[10, '20'],[10, '20'],[10, '20'],[10, '20'],[10, '20']);
+// console.log(a);
+// console.log(a);
+// console.log(a);
+// console.log(a);
+// console.log(a);
